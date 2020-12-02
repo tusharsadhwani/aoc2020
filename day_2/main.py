ef8e605349fb9d001453e0fad0608b03819d0f39
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-02 16:50:27 +0530

    Add day 2

---
+++ day_2/main.py
@@ -0,0 +1,82 @@
+"""--- Day 2: Password Philosophy ---
+Your flight departs in a few days from the coastal airport; the easiest
+way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad
+day. "Something's wrong with our computers; we can't log in!" You ask if
+you can take a look.
+
+Their password database seems to be a little corrupted: some of the
+passwords wouldn't have been allowed by the Official Toboggan Corporate
+Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle
+input) of passwords (according to the corrupted database) and the
+corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Each line gives the password policy and then the password. The password
+policy indicates the lowest and highest number of times a given letter
+must appear for the password to be valid. For example, 1-3 a means that
+the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg,
+is not; it contains no instances of b, but needs at least 1. The first
+and third passwords are valid: they contain one a or nine c, both within
+the limits of their respective policies.
+
+How many passwords are valid according to their policies?
+"""
+import re
+from typing import Tuple
+
+
+def get_password_values(line: str) -> Tuple[int, int, str, str]:
+    """Return first two numbers, character and password from input"""
+    match = re.match(r'^(\d+)-(\d+) ([a-z]): ([a-z]+)$', line)
+    assert match is not None, 'Invalid input file'
+
+    num1str, num2str, char, password = match.groups()
+    num1, num2 = int(num1str), int(num2str)
+
+    return num1, num2, char, password
+
+
+def part1() -> None:
+    """Solution to part 1"""
+    valid_count = 0
+
+    with open('input.txt') as infile:
+        for line in infile.readlines():
+            min_count, max_count, char, password = get_password_values(line)
+
+            char_count = password.count(char)
+            if min_count <= char_count <= max_count:
+                valid_count += 1
+
+    print(valid_count)
+
+
+def part2() -> None:
+    """Solution to part 2"""
+    valid_count = 0
+
+    with open('input.txt') as infile:
+        for line in infile.readlines():
+            index1, index2, char, password = get_password_values(line)
+
+            if password[index1-1] == char and password[index2-1] != char:
+                valid_count += 1
+            elif password[index1-1] != char and password[index2-1] == char:
+                valid_count += 1
+
+    print(valid_count)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
