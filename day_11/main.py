7b6c3ab13f49e0434b9640541e9f59ae04c9d9df
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-13 15:33:47 +0530

    Add day 11 part 2

--- day_11/main.py
+++ day_11/main.py
@@ -108,8 +108,136 @@ Once people stop moving around, you count 37 occupied seats.
 
 Simulate your seating area by applying the seating rules repeatedly
 until no seats change state. How many seats end up occupied?
+
+--- Part Two ---
+As soon as people start to arrive, you realize your mistake. People
+don't just care about adjacent seats - they care about the first seat
+they can see in each of those eight directions!
+
+Now, instead of considering just the eight immediately adjacent seats,
+consider the first seat in each of those eight directions. For example,
+the empty seat below would see eight occupied seats:
+
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....
+
+The leftmost empty seat below would only see one empty seat, but cannot
+see any of the occupied ones:
+
+.............
+.L.L.#.#.#.#.
+.............
+
+The empty seat below would see no occupied seats:
+
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.
+
+Also, people seem to be more tolerant than you expected: it now takes
+five or more visible occupied seats for an occupied seat to become empty
+(rather than four or more from the previous rules). The other rules
+still apply: empty seats that see no occupied seats become occupied,
+seats matching no rule don't change, and floor never changes.
+
+Given the same starting layout as above, these new rules cause the
+seating area to shift around as follows:
+
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##
+
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+Again, at this point, people stop shifting around and the seating area
+reaches equilibrium. Once this occurs, you count 26 occupied seats.
+
+Given the new visibility method and the rule change for occupied seats
+becoming empty, once equilibrium is reached, how many seats end up
+occupied?
 """
-from copy import deepcopy
 from typing import List
 
 
@@ -145,7 +273,7 @@ def part1() -> None:
         seats = [list(row) for row in infile.read().splitlines()]
 
     while True:
-        new_seats = deepcopy(seats)
+        new_seats = [row.copy() for row in seats]
         for i, row in enumerate(seats):
             for j, seat in enumerate(row):
                 if seat == '.':
@@ -165,5 +293,66 @@ def part1() -> None:
     print([seat == '#' for row in seats for seat in row].count(True))
 
 
+def find_neighbours(seats: List[List[str]], row: int, col: int) -> int:
+    """Find all neighbouring seats according to part 2"""
+    rows = len(seats)
+    cols = len(seats[0])
+
+    neighbour_count = 0
+
+    for i in -1, 0, 1:
+        for j in -1, 0, 1:
+            if i == j == 0:
+                continue
+
+            new_row = row
+            new_col = col
+
+            while True:
+                new_row = new_row + i
+                new_col = new_col + j
+
+                if new_row < 0 or new_row >= rows:
+                    break
+                if new_col < 0 or new_col >= cols:
+                    break
+
+                if seats[new_row][new_col] == '#':
+                    neighbour_count += 1
+                    break
+
+                if seats[new_row][new_col] == 'L':
+                    break
+
+    return neighbour_count
+
+
+def part2() -> None:
+    """Solution for part 2"""
+    with open('input.txt') as infile:
+        seats = [list(row) for row in infile.read().splitlines()]
+
+    while True:
+        new_seats = [row.copy() for row in seats]
+        for i, row in enumerate(seats):
+            for j, seat in enumerate(row):
+                if seat == '.':
+                    continue
+
+                neighbours = find_neighbours(seats, i, j)
+                if neighbours == 0:
+                    new_seats[i][j] = '#'
+                elif neighbours >= 5:
+                    new_seats[i][j] = 'L'
+
+        if new_seats == seats:
+            break
+
+        seats = new_seats
+
+    print([seat == '#' for row in seats for seat in row].count(True))
+
+
 if __name__ == "__main__":
     part1()
+    part2()
