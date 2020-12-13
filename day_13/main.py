e54dbddd93caa830170052571906e7c4ce08c209
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-13 22:17:21 +0530

    Add day 13 part 1

---
+++ day_13/main.py
@@ -0,0 +1,102 @@
+"""--- Day 13: Shuttle Search ---
+Your ferry can make it safely to a nearby port, but it won't get much
+further. When you call to book another ship, you discover that no ships
+embark from that port to your vacation island. You'll need to get from
+the port to the nearest airport.
+
+Fortunately, a shuttle bus service is available to bring you from the
+sea port to the airport! Each bus has an ID number that also indicates
+how often the bus leaves for the airport.
+
+Bus schedules are defined based on a timestamp that measures the number
+of minutes since some fixed reference point in the past. At timestamp 0,
+every bus simultaneously departed from the sea port. After that, each
+bus travels to the airport, then various other locations, and finally
+returns to the sea port to repeat its journey forever.
+
+The time this loop takes a particular bus is also its ID number: the bus
+with ID 5 departs from the sea port at timestamps 0, 5, 10, 15, and so
+on. The bus with ID 11 departs at 0, 11, 22, 33, and so on. If you are
+there when the bus departs, you can ride that bus to the airport!
+
+Your notes (your puzzle input) consist of two lines. The first line is
+your estimate of the earliest timestamp you could depart on a bus. The
+second line lists the bus IDs that are in service according to the
+shuttle company; entries that show x must be out of service, so you
+decide to ignore them.
+
+To save time once you arrive, your goal is to figure out the earliest
+bus you can take to the airport. (There will be exactly one such bus.)
+
+For example, suppose you have the following notes:
+
+939
+7,13,x,x,59,x,31,19
+
+Here, the earliest timestamp you could depart is 939, and the bus IDs in
+service are 7, 13, 59, 31, and 19. Near timestamp 939, these bus IDs
+depart at the times marked D:
+
+time   bus 7   bus 13  bus 59  bus 31  bus 19
+929      .       .       .       .       .
+930      .       .       .       D       .
+931      D       .       .       .       D
+932      .       .       .       .       .
+933      .       .       .       .       .
+934      .       .       .       .       .
+935      .       .       .       .       .
+936      .       D       .       .       .
+937      .       .       .       .       .
+938      D       .       .       .       .
+939      .       .       .       .       .
+940      .       .       .       .       .
+941      .       .       .       .       .
+942      .       .       .       .       .
+943      .       .       .       .       .
+944      .       .       D       .       .
+945      D       .       .       .       .
+946      .       .       .       .       .
+947      .       .       .       .       .
+948      .       .       .       .       .
+949      .       D       .       .       .
+
+The earliest bus you could take is bus ID 59. It doesn't depart until
+timestamp 944, so you would need to wait 944 - 939 = 5 minutes before
+it departs. Multiplying the bus ID by the number of minutes you'd need
+to wait gives 295.
+
+What is the ID of the earliest bus you can take to the airport
+multiplied by the number of minutes you'll need to wait for that bus?
+"""
+from typing import List, Tuple
+
+
+def parse_input() -> Tuple[int, List[int]]:
+    """Parses input to return arrival time and bus timings"""
+    with open('input.txt') as infile:
+        arrival_time = int(infile.readline())
+        bus_timings = [int(x)
+                       for x in infile.readline().split(',')
+                       if x != 'x']
+
+    return arrival_time, bus_timings
+
+
+def part1() -> None:
+    """Solution for part 1"""
+    arrival_time, bus_timings = parse_input()
+
+    waiting_times = [x - (arrival_time % x) for x in bus_timings]
+    min_waiting_time = waiting_times[0]
+    min_waiting_index = 0
+    for index, time in enumerate(waiting_times):
+        if min_waiting_time > time:
+            min_waiting_time = time
+            min_waiting_index = index
+
+    bus_id = bus_timings[min_waiting_index]
+    print(bus_id * min_waiting_time)
+
+
+if __name__ == "__main__":
+    part1()
